dfe41d37743662418f642018f603c43b24a2e9b1
Lebogang <lololebogang9-design@gmail.com>
2026-06-19 13:26:04 +0200

    doing my enquiry form to have a name , a valid email and so they put in the message but it seems to not be working

---
+++ script.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('enquiry-form');
+const messageBox = document.getElementById('message-box');
+
+// define the code that will run as soon as user clicks submit button
+// click submit button
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // prevent the page from refreshing
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (name== "") {
+        messageBox.textContent = "Please enter your name.";
+        return;
+    }
+    if (email == "") {
+        messageBox.textContent = "Please enter your email.";
+        return;
+    }
+    if (email.includes("@") || email.includes(".")) {
+        messageBox.textContent = "Please enter a valid email address.";
+        return;
+    }
+    if (message == "") {
+        messageBox.textContent = "Please state your enquiry ";
+        return;
+    }
+    const myEmail = "lololeleb@gmail.com";
+    const subject = "New Enquiry from " + name;
+    const body = "Name: " + name + "\nEmail: " + email + "\nMessage: " + message;
+
+    window.location.href = 
+        "mailto:" + myEmail + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
+
+    //if all is well 
+    messageBox.textContent = "Your enquiry has been sent successfully!";    
+})
